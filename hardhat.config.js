fd88906e8ecc45495c59ebae920229fc9a3db9e0
mitoyouhei <mitoyouhei@proton.me>
2023-02-03 02:36:57 +0800

    enable deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    ganache: {
+      url: process.env.GANACHE_URL,
+      accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
