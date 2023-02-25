2e84aacad908312634a113a002a292624f6d9b7f
mitoyouhei <mitoyouhei@proton.me>
2023-02-25 17:19:05 +0800

    Update Game structure.

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -1,109 +1,89 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.19 <0.9.0;
-
-contract DiceGame {
-    address private player1;
-    address private player2;
-    uint private betAmount;
-    uint private player1BetNumber;
-    uint private player2BetNumber;
-
-    function createGame(uint betNumber) public payable {
-        require(msg.value > 0, "Please send a bet amount.");
-        require(player1 == address(0), "Game already created.");
-        require(
-            betNumber >= 1 && betNumber <= 6,
-            "Please send correct number."
-        );
-        player1 = msg.sender;
-        betAmount = msg.value;
-        player1BetNumber = betNumber;
-    }
-
-    function joinGame(uint betNumber) public payable {
-        require(msg.value == betAmount, "Please send the correct bet amount.");
-        require(player2 == address(0), "Game already has two players.");
-        require(player1 != msg.sender, "Cannot play with same account.");
-        require(
-            betNumber >= 1 && betNumber <= 6,
-            "Please send correct number."
-        );
-        player2 = msg.sender;
-        player2BetNumber = betNumber;
-    }
-
-    function rollDice() public returns (uint) {
-        uint roll = (uint(
-            keccak256(
-                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
-            )
-        ) % 6) + 1;
-        bool isBig = roll >= 4;
-
-        address bigWinner;
-        address smallWinner;
-        if (player1BetNumber >= 4) {
-            bigWinner = player1;
-            smallWinner = player2;
-        } else {
-            bigWinner = player2;
-            smallWinner = player1;
-        }
-
-        if (isBig) {
-            payWinner(bigWinner);
-        } else {
-            payWinner(smallWinner);
-        }
-
-        return roll;
-    }
-
-    function payWinner(address winner) private {
-        payable(winner).transfer(address(this).balance);
-    }
-
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
-
-    function getAddress() public view returns (address) {
-        return address(this);
-    }
-}
+import "hardhat/console.sol";
 
 contract DiceGameLobby {
-    uint256 private gameCounter;
+    struct DiceGame {
+        uint256 id;
+        address player1;
+        address player2;
+        uint256 betAmount;
+        uint256 player1BetNumber;
+        uint256 player2BetNumber;
+    }
+    uint256 gameIndexFeed;
     DiceGame[] private games;
-    event CreateGame(address _gameAddress);
-    event PlayGame(uint _rollNumber);
 
-    function createGame(uint betNumber) public payable {
-        require(msg.value > 0, "Please send a bet amount.");
-        DiceGame newGame = (new DiceGame)();
-        newGame.createGame{value: msg.value}(betNumber);
-        games.push(newGame);
-        gameCounter++;
-        emit CreateGame(newGame.getAddress());
+    event CreateGame(uint256 id);
+    event PlayGame(uint256 roll);
+
+    function getGames() public view returns (DiceGame[] memory) {
+        return games;
     }
 
-    function play(address gameAddress, uint betNumber) public payable {
+    function createGame(uint256 betNumber) public payable {
         require(msg.value > 0, "Please send a bet amount.");
+        DiceGame memory newGame = DiceGame({
+            id: ++gameIndexFeed,
+            player1: msg.sender,
+            player1BetNumber: betNumber,
+            betAmount: msg.value,
+            player2: address(0),
+            player2BetNumber: 0
+        });
+        games.push(newGame);
+        emit CreateGame(newGame.id);
+    }
+
+    function play(uint256 id, uint256 betNumber) public payable {
         for (uint256 i = 0; i < games.length; i++) {
-            DiceGame game = games[i];
-            if (game.getAddress() == gameAddress) {
-                game.joinGame{value: msg.value}(betNumber);
-                uint roll = game.rollDice();
+            if (games[i].id == id) {
+                DiceGame memory game = games[i];
+                require(
+                    msg.value == game.betAmount,
+                    "Please send the correct bet amount."
+                );
+
+                game.player2 = msg.sender;
+                game.player2BetNumber = betNumber;
+
+                uint256 roll = rollDice();
+                console.log("roll", roll);
+
+                bool isBig = roll >= 4;
+
+                address bigWinner;
+                address smallWinner;
+                if (game.player1BetNumber >= 4) {
+                    bigWinner = game.player1;
+                    smallWinner = game.player2;
+                } else {
+                    bigWinner = game.player2;
+                    smallWinner = game.player1;
+                }
+
+                if (isBig) {
+                    payWinner(bigWinner, game.betAmount * 2);
+                } else {
+                    payWinner(smallWinner, game.betAmount * 2);
+                }
+
                 emit PlayGame(roll);
             }
         }
     }
 
-    function getGameCount() public view returns (uint) {
-        return gameCounter;
+    function payWinner(address winner, uint256 balance) private {
+        payable(winner).transfer(balance);
     }
 
-    function getGames() public view returns (DiceGame[] memory) {
-        return games;
+    function rollDice() private view returns (uint256) {
+        uint256 roll = (uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
+            )
+        ) % 6) + 1;
+        console.log("rollDice roll", roll);
+        return roll;
     }
 }
