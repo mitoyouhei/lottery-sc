546170f7bfd08a72ddab017c24c6ec0e22907687
Satoru <unknown>
2023-02-27 19:30:28 +0800

    Add UT for Dice

--- test/Dice.js
+++ test/Dice.js
@@ -25,12 +25,13 @@ describe("DiceGame", function () {
     player2BetNum = 6;
   });
 
-  // 测试 createGame 函数
+  // 测试 createGame&play 函数
   it("should allow players to create and play the game", async function () {
     // 玩家 1 加入游戏
     await diceGame.createGame(player1BetNum, { value: ethers.utils.parseEther("1") });
     const game = (await diceGame.getGames())[0];
     expect(game.player1).to.equal(player1.address);
+    expect(game.player2).to.equal(emptyAddress);
     expect(game.player1BetNumber).to.equal(player1BetNum);
 
     // 玩家 2 加入游戏
@@ -40,48 +41,22 @@ describe("DiceGame", function () {
     const gameAfterPlay = (await diceGame.getGames())[0];
     expect(gameAfterPlay.player2).to.equal(player2.address);
   });
-//
-//  // 测试 rollDice 函数
-//  it("should allow players to roll the dice", async function () {
-//    // 玩家 1 和玩家 2 加入游戏
-//    await diceGame.joinGame({ value: ethers.utils.parseEther("1") });
-//    await diceGame
-//      .connect(player2)
-//      .joinGame({ value: ethers.utils.parseEther("1") });
-//
-//    // 玩家 1 和玩家 2 分别掷骰子
-//    await diceGame.rollDice();
-//    await diceGame.connect(player2).rollDice();
-//
-//    // 检查游戏是否结束
-//    expect(await diceGame.gameFinished()).to.equal(true);
-//
-//    // 检查胜者地址是否有效
-//    const winner = await diceGame.determineWinner();
-//    expect(winner).to.not.equal(emptyAddress);
-//  });
-//
-//  // 测试 payout 函数
-//  it("should allow the winner to withdraw their winnings", async function () {
-//    // 玩家 1 和玩家 2 加入游戏
-//    await diceGame.joinGame({ value: ethers.utils.parseEther("1") });
-//    await diceGame
-//      .connect(player2)
-//      .joinGame({ value: ethers.utils.parseEther("1") });
-//
-//    // 玩家 1 和玩家 2 分别掷骰子
-//    await diceGame.rollDice();
-//    await diceGame.connect(player2).rollDice();
-//
-//    // 确定胜者并进行付款
-//    const winner = await diceGame.determineWinner();
-//    await expect(() => diceGame.payout()).to.changeEtherBalance(
-//      winner,
-//      ethers.utils.parseEther("2")
-//    );
-//  });
-});
 
-// // 定义 DiceGameLobby 合约测试套件
-// describe("DiceGameLobby", function () {
-//   // 声明全局变量
+  // 测试 createGame&play 函数
+  it("should show error when betAmount are different", async function () {
+    const player1BetAmount = "1";
+    const player2BetAmount = "2";
+        // 玩家 1 加入游戏
+    await diceGame.createGame(player1BetNum, { value: ethers.utils.parseEther(player1BetAmount) });
+    const game = (await diceGame.getGames())[0];
+      // 玩家 2 加入游戏
+    try {
+        const result = await diceGame
+          .connect(player2)
+          .play(game.id, player2BetNum, { value: ethers.utils.parseEther(player2BetAmount) });
+  　    expect(result).to.be.null;
+    } catch (err) {
+    　  expect(err).to.not.be.null;
+    }
+  });
+});
