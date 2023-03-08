b7058ab0796b715680c3727c32d29ed0d4908cac
Satoru <unknown>
2023-03-08 20:42:23 +0800

    Add New Dice Game for VRF

---
+++ contracts/DiceWithVRF.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.18 <0.9.0;
+import "hardhat/console.sol";
+
+import "./BankRoll.sol";
+import "./Game.sol";
+
+contract DiceWithVRF is Game {
+    // 掷骰子游戏
+    // 选项: 点数, 1~6;
+    function init(uint256 customizeWager) public override {
+        super.init(customizeWager);
+        gameType = "DICE";
+    }
+    
+    function getWinnerAndLoser()
+    public
+    view
+    override
+    returns (address, address)
+    {
+        require(gamblers.length == 2, "NEED_TWO_PLAYER");
+        Gambler memory gamblerA = gamblers[0];
+        Gambler memory gamblerB = gamblers[1];
+        
+        uint256 roll = (uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
+            )
+        ) % 6) + 1;
+        
+        bool winnerIsBig = roll >= 4;
+        bool gamblerBIsBig = gamblerB.bet >= 4;
+        bool gamblerBIsWinner = (winnerIsBig && gamblerBIsBig) ||
+        (!winnerIsBig && !gamblerBIsBig);
+        
+        return
+        gamblerBIsWinner
+        ? (gamblerB.id, gamblerA.id)
+        : (gamblerA.id, gamblerB.id);
+    }
+    
+    function getDisplayInfo()
+    public
+    view
+    override
+    returns (DisplayInfo memory)
+    {
+        return
+        DisplayInfo({
+        id: address(this),
+        wager: wager,
+        gameType: gameType,
+        gamblers: gamblers
+        });
+    }
+}
