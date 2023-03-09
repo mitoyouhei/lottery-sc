63e4f45e5d0a4bb0498c82c69942120ea7d89bad
Satoru <unknown>
2023-03-09 20:57:30 +0800

    Demo VRF

--- contracts/DiceWithVRF.sol
+++ contracts/DiceWithVRF.sol
@@ -2,56 +2,83 @@
 pragma solidity >=0.8.18 <0.9.0;
 import "hardhat/console.sol";
 
-import "./BankRoll.sol";
-import "./Game.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract DiceWithVRF is Game {
-    // 掷骰子游戏
-    // 选项: 点数, 1~6;
-    function init(uint256 customizeWager) public override {
-        super.init(customizeWager);
-        gameType = "DICE";
+uint32 constant CALLBACK_GAS_LIMIT = 150000;
+uint32 constant NUM_WORDS = 1;
+uint16 constant REQUEST_CONFIRMATIONS = 3;
+uint64 constant SUBSCRIPTION_ID = 10593;
+address constant LINK_TOKEN = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB; // GOERLI TEST
+bytes32 constant KEY_HASH = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15; // we alread set this
+
+contract DiceWithVRF is VRFConsumerBaseV2, ConfirmedOwner {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+    public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+    // past requests Id.
+    uint256[] public requestIds;
+    // address public immutable linkToken;
+    uint public randomWordsNum;
+    
+    constructor()
+    VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+    ConfirmedOwner(msg.sender)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
     }
     
-    function getWinnerAndLoser()
-    public
-    view
-    override
-    returns (address, address)
-    {
-        require(gamblers.length == 2, "NEED_TWO_PLAYER");
-        Gambler memory gamblerA = gamblers[0];
-        Gambler memory gamblerB = gamblers[1];
-        
-        uint256 roll = (uint256(
-            keccak256(
-                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
-            )
-        ) % 6) + 1;
-        
-        bool winnerIsBig = roll >= 4;
-        bool gamblerBIsBig = gamblerB.bet >= 4;
-        bool gamblerBIsWinner = (winnerIsBig && gamblerBIsBig) ||
-        (!winnerIsBig && !gamblerBIsBig);
-        
-        return
-        gamblerBIsWinner
-        ? (gamblerB.id, gamblerA.id)
-        : (gamblerA.id, gamblerB.id);
-    }
-    
-    function getDisplayInfo()
-    public
-    view
-    override
-    returns (DisplayInfo memory)
-    {
-        return
-        DisplayInfo({
-        id: address(this),
-        wager: wager,
-        gameType: gameType,
-        gamblers: gamblers
+    function requestRandomWords() public onlyOwner returns (uint256 requestId) {
+        console.log("requestRandomWords");
+
+        requestId = COORDINATOR.requestRandomWords(
+            KEY_HASH,
+            SUBSCRIPTION_ID,
+            REQUEST_CONFIRMATIONS,
+            CALLBACK_GAS_LIMIT,
+            NUM_WORDS
+        );
+        console.log("requestRandomWords");
+        console.log("requestRandomWords requestId: ", requestId);
+
+        s_requests[requestId] = RequestStatus({
+        randomWords: new uint256[](0),
+        exists: true,
+        fulfilled: false
         });
+        requestIds.push(requestId);
+        emit RequestSent(requestId, NUM_WORDS);
+        return requestId; // requestID is a uint.
+    }
+    
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        console.log("fulfillRandomWords requestId: ", _requestId);
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        randomWordsNum = _randomWords[0]; // Set array-index to variable, easier to play with
+        console.log("fulfillRandomWords randomWordsNum: ", randomWordsNum);
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+    
+    // to check the request status of random number call.
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+    
+    function getRequestId() public view returns (uint256[] memory){
+        return requestIds;
     }
 }
