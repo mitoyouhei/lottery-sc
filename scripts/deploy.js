e95ca23e5ae0d201f580c92cc36f140d2ffa1e80
Satoru <unknown>
2023-03-18 17:40:24 +0800

    Manage Contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,38 @@
 const { ethers, network, upgrades } = require("hardhat");
+const { getAddress } = require("../addressManage");
 
-// TODO: real goerli address, and address management
-const originalAddress = '0x0Fcb48a255bC37c508d1093731320bd40d4B3288';
-const mumbaiTestNetAddress = '0x7e9BA8A648B3850b87Be4e3956F9388950c3d86A';
 async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts...");
-  console.log("Network:", network.name);
-  console.log("Account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Network: ", network.name);
+  console.log("Account: ", deployer.address);
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
 
   // 原始部署
   // const Casino = await ethers.getContractFactory("Casino");
   // const casino = await Casino.deploy();
 
-  // 第一次部署
-  // const Casino = await ethers.getContractFactory("Casino");
-  // const casino = await upgrades.deployProxy(Casino,{ initializer: 'init' })
-
-  // 后续部署
+  const originalAddress = getAddress();
   const Casino = await ethers.getContractFactory("Casino");
-  const casino = await upgrades.upgradeProxy(mumbaiTestNetAddress, Casino);
-  console.log(mumbaiTestNetAddress," box(proxy) address");
+  let casino;
 
-  console.log(await upgrades.erc1967.getImplementationAddress(casino.address)," getImplementationAddress");
-  console.log(await upgrades.erc1967.getAdminAddress(casino.address)," getAdminAddress");
+  console.log('originalAddress', originalAddress);
 
-  console.log("Casino address:", casino.address);
+  if(originalAddress) {
+    // 后续部署
+    console.log('Not first time deploy');
+    casino = await upgrades.upgradeProxy(originalAddress, Casino);
+  } else {
+    // 第一次部署
+    console.log('First time deploy');
+    casino = await upgrades.deployProxy(Casino,{ initializer: 'init' });
+  }
+
+  console.log("ImplementationAddress: ", await upgrades.erc1967.getImplementationAddress(casino.address));
+  console.log("AdminAddress: ", await upgrades.erc1967.getAdminAddress(casino.address));
+  console.log("Casino address: ", casino.address);
+  console.log("Contracts deployed");
 }
 
 main()
