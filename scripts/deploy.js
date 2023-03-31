a11647927b61fdbe9e0797905338007206cbb926
Satoru <unknown>
2023-03-31 11:36:39 +0800

    Revert "Contract upgrade script update"
    This reverts commit 13d536f89b65fb29e0ff71a01cd946a51a8b146c.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 const { ethers, network, upgrades } = require("hardhat");
 
 // TODO: real goerli address, and address management
-const originalAddress = '0x0Fcb48a255bC37c508d1093731320bd40d4B3288'
+const originalAddress = '0x0Fa71a9Ba8dD2838A26f0290AC79251B1929890d'
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -11,19 +11,15 @@ async function main() {
   console.log("Account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // 原始部署
-  // const Casino = await ethers.getContractFactory("Casino");
-  // const casino = await Casino.deploy();
+  // const FundMe = await ethers.getContractFactory("FundMe");
+  // const fundMe = await FundMe.deploy();
 
-  // 第一次部署
-  // const Casino = await ethers.getContractFactory("Casino");
-  // const casino = await upgrades.deployProxy(Casino,[], { initializer: 'init' })
+  // console.log("FundMe address:", fundMe.address);
 
-  // 后续部署
   const Casino = await ethers.getContractFactory("Casino");
   const casino = await upgrades.upgradeProxy(originalAddress, Casino);
-  console.log(originalAddress," box(proxy) address");
 
+  console.log(originalAddress," box(proxy) address");
   console.log(await upgrades.erc1967.getImplementationAddress(casino.address)," getImplementationAddress");
   console.log(await upgrades.erc1967.getAdminAddress(casino.address)," getAdminAddress");
 
