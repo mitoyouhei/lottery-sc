8bce13d98a2b82f2392c47819743837d838f237e
Satoru <unknown>
2023-03-31 11:36:35 +0800

    Revert "Commit openzeppelin config file."
    This reverts commit 8249fe7cf871fc57dec5fe23b4423e3b3ad870d4.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,8 @@
 const { ethers, network, upgrades } = require("hardhat");
 
 // TODO: real goerli address, and address management
-const originalAddress = '0x0Fcb48a255bC37c508d1093731320bd40d4B3288';
-const mumbaiTestNetAddress = '0x7e9BA8A648B3850b87Be4e3956F9388950c3d86A';
+const originalAddress = '0x0Fcb48a255bC37c508d1093731320bd40d4B3288'
+
 async function main() {
   const [deployer] = await ethers.getSigners();
 
@@ -17,12 +17,12 @@ async function main() {
 
   // 第一次部署
   // const Casino = await ethers.getContractFactory("Casino");
-  // const casino = await upgrades.deployProxy(Casino,{ initializer: 'init' })
+  // const casino = await upgrades.deployProxy(Casino,[], { initializer: 'init' })
 
   // 后续部署
   const Casino = await ethers.getContractFactory("Casino");
-  const casino = await upgrades.upgradeProxy(mumbaiTestNetAddress, Casino);
-  console.log(mumbaiTestNetAddress," box(proxy) address");
+  const casino = await upgrades.upgradeProxy(originalAddress, Casino);
+  console.log(originalAddress," box(proxy) address");
 
   console.log(await upgrades.erc1967.getImplementationAddress(casino.address)," getImplementationAddress");
   console.log(await upgrades.erc1967.getAdminAddress(casino.address)," getAdminAddress");
