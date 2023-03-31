8e0c5dd023b9529406adffbd33c29e8fdf10e3d9
Satoru <unknown>
2023-03-31 11:36:16 +0800

    Revert "Update network name with test suffixes"
    This reverts commit cd79149ec1410e57907da15a6158fe9d1d92babd.

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,11 +11,11 @@ module.exports = {
       url: process.env.GANACHE_URL,
       accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
     },
-    goerli_test: {
+    goerli: {
       url: process.env.GOERLI_URL,
       accounts: [process.env.GOERLI_DEPLOY_ACCOUNT_PRIVATE_KEY],
     },
-    mumbai_test: {
+    mumbai: {
       url: process.env.MUMBAI_URL,
       accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT_PRIVATE_KEY],
     },
