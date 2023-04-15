194a8d4dfb71c0814383acb51c228006ba95aee3
Satoru <unknown>
2023-04-15 13:56:37 +0800

    Update config with correct structure.

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,29 +13,29 @@ module.exports = {
         runs: 1000, // default 200
       },
     },
-    networks: {
-      ganache: {
-        url: process.env.GANACHE_URL,
-        accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
-      },
-      goerli: {
-        url: process.env.GOERLI_URL,
-        accounts: [process.env.GOERLI_DEPLOY_ACCOUNT_PRIVATE_KEY],
-      },
-      mumbai: {
-        url: process.env.MUMBAI_URL,
-        accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT_PRIVATE_KEY],
-      },
-      polygon: {
-        url: process.env.POLYGON_URL,
-        accounts: [process.env.POLYGON_DEPLOY_ACCOUNT_PRIVATE_KEY],
-      },
+  },
+  networks: {
+    ganache: {
+      url: process.env.GANACHE_URL,
+      accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
     },
-    contractSizer: {
-      alphaSort: true,
-      disambiguatePaths: false,
-      runOnCompile: true,
-      strict: true,
-    }
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.GOERLI_DEPLOY_ACCOUNT_PRIVATE_KEY],
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT_PRIVATE_KEY],
+    },
+    polygon: {
+      url: process.env.POLYGON_URL,
+      accounts: [process.env.POLYGON_DEPLOY_ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
   }
 }
