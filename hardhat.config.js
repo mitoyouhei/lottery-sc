a677469990d21a17db9df05d877aba7225febc66
Satoru <unknown>
2023-04-15 13:14:07 +0800

    Fix module export

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,30 +11,31 @@ module.exports = {
       optimizer: {
         enabled: true,
         runs: 1000, // default 200
+      },
     },
-  },
-  networks: {
-    ganache: {
-      url: process.env.GANACHE_URL,
-      accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
+    networks: {
+      ganache: {
+        url: process.env.GANACHE_URL,
+        accounts: [process.env.GANACHE_DEPLOY_ACCOUNT_PRIVATE_KEY],
+      },
+      goerli: {
+        url: process.env.GOERLI_URL,
+        accounts: [process.env.GOERLI_DEPLOY_ACCOUNT_PRIVATE_KEY],
+      },
+      mumbai: {
+        url: process.env.MUMBAI_URL,
+        accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT_PRIVATE_KEY],
+      },
+      polygon: {
+        url: process.env.POLYGON_URL,
+        accounts: [process.env.POLYGON_DEPLOY_ACCOUNT_PRIVATE_KEY],
+      },
     },
-    goerli: {
-      url: process.env.GOERLI_URL,
-      accounts: [process.env.GOERLI_DEPLOY_ACCOUNT_PRIVATE_KEY],
-    },
-    mumbai: {
-      url: process.env.MUMBAI_URL,
-      accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT_PRIVATE_KEY],
-    },
-    polygon: {
-      url: process.env.POLYGON_URL,
-      accounts: [process.env.POLYGON_DEPLOY_ACCOUNT_PRIVATE_KEY],
-    },
-  },
-  contractSizer: {
-    alphaSort: true,
-    disambiguatePaths: false,
-    runOnCompile: true,
-    strict: true,
+    contractSizer: {
+      alphaSort: true,
+      disambiguatePaths: false,
+      runOnCompile: true,
+      strict: true,
+    }
   }
-};
+}
